df499783c220c74c43511252ee11e74abcd19f76
Gustavo Bertolini <Gustavo.Grzadziel@outlook.com>
2017-12-06 13:44:43 -0200

    This update added a whole new world of options! -Now, your settings to generate a password are saved, and the next time that you visit us, we'll remember how to generate your password! -New way to generate passwords: now, you can opt for add symbols and numbers in a way that you can remember more easily. -Page optimization! Now, the page loads faster than before, thanks to caching and a lot of code refactoring. -Improvement in meta tags! We will now be found on Google (hopefully). -Preparations for a future update, where you'll be able to see your password beeing generated.

--- js/interface.js
+++ js/interface.js
@@ -1,56 +1,79 @@
 isNumbersAppearing = true;
-function showNumbersSpot(){
-	$("#addNumbersLocation").show();
-	isNumbersAppearing = true;
-	$("#checkboxNumbers").bootstrapToggle("on");
-	}
-
-function hideNumberSpot(){
-	$("#addNumbersLocation").hide();
-	isNumbersAppearing = false;	
-	$("#checkboxNumbers").bootstrapToggle("off");
+function showNumbersSpot() {
+    document.getElementById("addNumbersLocation").style.display = '';
+    isNumbersAppearing = true;
+    $("#checkboxNumbers").bootstrapToggle("on");
 }
 
-$(document).ready(function() {
-	$("#hidenSymbolsMenu").hide();
-	//On checkbox click, in case of turning on, show the symbols menu.
-	//In case of turning off, hide the symbols menu, and show the numbers option
-	$("#checkboxSymbol").change(function () {
-        var isChecked = $("#checkboxSymbol").is(":checked");
-		if (isChecked){
-			$("#hidenSymbolsMenu").show();
-					}
-		else{
-			$("#hidenSymbolsMenu").hide();
-			if (!isNumbersAppearing){
-				showNumbersSpot();
-			}
-			}
-        $("#radioSymbolUserDefined").prop("checked", true);
+function hideNumberSpot() {
+    document.getElementById("addNumbersLocation").style.display = 'none';
+    isNumbersAppearing = false;
+    $("#checkboxNumbers").bootstrapToggle("off");
+}
 
-	});
-	
-	
-    $("#checkboxNumbers").change(function () {
-        $("#addNumbersInput").prop("disabled", !$("#checkboxNumbers").is(":checked"));
-        //$("#addNumbersInput").val($("#checkboxNumbers").is(":checked") ? 2 : null)
-	});	
-	
-    $("#radioSymbolUserDefined").change(function () {
-		var isChecked = $("#radioSymbolUserDefined").is(":checked");
-		if (isChecked){
-			$("#addSymbolsInput").prop("disabled", false);
-			showNumbersSpot();
-			}
-	});
-	
-	$("#radioSymbolSystemDefined").change(function () {
-		var isChecked = $("#radioSymbolSystemDefined").is(":checked");
-		if (isChecked){
-			$("#addSymbolsInput").prop("disabled", true);
-			hideNumberSpot();
-		}
-			
-	});
-	
+
+function symbolChange() {
+    var isChecked = document.getElementById("checkboxSymbol").checked;
+    if (isChecked)
+        document.getElementById("hidenSymbolsMenu").style.display = '';
+    else {
+        document.getElementById("hidenSymbolsMenu").style.display = 'none';
+        if (!isNumbersAppearing)
+            showNumbersSpot();
+    }
+    $("#radioSymbolUserDefined").prop("checked", true);
+}
+function radioChange() {
+    var isChecked = $("#radioSymbolUserDefined").is(":checked");
+    if (isChecked) {
+        document.getElementById("addSymbolsInput").disabled = false;
+        showNumbersSpot();
+    }
+    else {
+        document.getElementById("addSymbolsInput").disabled = true;
+        hideNumberSpot();
+    }
+}
+function storeData() {
+    localStorage.setItem('hasData', true);
+    localStorage.setItem('capitalizationOption', document.getElementById('capitalizationSelect').value);
+    localStorage.setItem('numbersQuantity', parseInt(document.getElementById("addNumbersInput").value));
+    localStorage.setItem('generateWithSymbols', document.getElementById("checkboxSymbol").checked);
+    localStorage.setItem('symbolsScheme', parameters.generateWithSymbols ? document.querySelector('input[name=radioSymbol]:checked').value : null);
+    localStorage.setItem('symbolsToAdd', document.getElementById("addSymbolsInput").value);
+    localStorage.setItem('generateWithNumbers', document.getElementById("checkboxNumbers").checked);
+    localStorage.setItem('WN_minLength', parseInt(document.getElementById("lengthFrom").value));
+    localStorage.setItem('WN_maxLength', parseInt(document.getElementById("lengthTo").value));
+    localStorage.setItem('WN_limit', parseInt(document.getElementById("wordQuantity").value));
+}
+
+function retrieveData() {
+    document.getElementById('capitalizationSelect').value = localStorage.getItem('capitalizationOption');
+    document.getElementById('addNumbersInput').value = localStorage.getItem('numbersQuantity');
+    localStorage.getItem('generateWithSymbols') === "true" ? $("#checkboxSymbol").bootstrapToggle("on") : $("#checkboxSymbol").bootstrapToggle("off");
+    symbolChange();
+    document.getElementById('addSymbolsInput').value = localStorage.getItem('symbolsToAdd');
+    localStorage.getItem('generateWithNumbers') === "true" ? $("#checkboxNumbers").bootstrapToggle("on") : $("#checkboxNumbers").bootstrapToggle("off");
+    document.getElementById('lengthFrom').value = localStorage.getItem('WN_minLength');
+    document.getElementById('lengthTo').value = localStorage.getItem('WN_maxLength');
+    document.getElementById('wordQuantity').value = localStorage.getItem('WN_limit');
+    if (localStorage.getItem('symbolsScheme') === 'userDefined')
+        document.getElementById('radioSymbolUserDefined').checked = true;
+    else if (localStorage.getItem('symbolsScheme') === 'systemDefined')
+        document.getElementById('radioSymbolSystemDefined').checked = true;
+    radioChange();
+}
+
+$(document).ready(function () {
+    document.getElementById("hidenSymbolsMenu").style.display = 'none';
+    if (localStorage.getItem('hasData'))
+        retrieveData();
+
+    $("#checkboxSymbol").change(function () { symbolChange(); });
+
+    $("#checkboxNumbers").change(function () { document.getElementById("addNumbersInput").disabled = !document.getElementById("checkboxNumbers").checked; });
+
+    $("#radioSymbolUserDefined").change(function () { radioChange(); });
+    $("#radioSymbolSystemDefined").change(function () { radioChange(); });
+
 });
--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
  * @param {string} word Word to randomically capitalize.
  * @returns {string} Word randomically capitalized. 
  */
- //#region randomize
+//#region randomize
 function randomizeCapitalization(word) {
     for (i = 0; i < word.length; i++) {
         if (Math.random() > 0.5)
@@ -34,18 +34,18 @@ function randomizeArray(oldArray) {
 //#region getData
 function getParameters() {
     parameters = {};
-    parameters.capitalizationOption = $("#capitalizationSelect").val();
-    parameters.numbersQuantity = parseInt($("#addNumbersInput").val());
-    parameters.generateWithSymbols = $("#checkboxSymbol").is(":checked");
-	parameters.symbolsScheme = parameters.generateWithSymbols ? $('input[name=radioSymbol]:checked').val() : null;
-    parameters.symbolsToAdd = parameters.symbolsScheme === "userDefined" ? $("#addSymbolsInput").val().split(",") : null;
-    parameters.generateWithNumbers = $("#checkboxNumbers").is(":checked");
+    parameters.capitalizationOption = document.getElementById('capitalizationSelect').value;
+    parameters.numbersQuantity = parseInt(document.getElementById("addNumbersInput").value);
+    parameters.generateWithSymbols = document.getElementById("checkboxSymbol").checked;
+    parameters.symbolsScheme = parameters.generateWithSymbols ? document.querySelector('input[name=radioSymbol]:checked').value : null;
+    parameters.symbolsToAdd = parameters.symbolsScheme === "userDefined" ? document.getElementById("addSymbolsInput").value.split(",") : null;
+    parameters.generateWithNumbers = document.getElementById("checkboxNumbers").checked;
     parameters.wordnikParameters = {
         hasDictionaryDef: "true",
         includePartOfSpeech: "noun",
-        minLength: parseInt($("#lengthFrom")[0].value),
-        maxLength: parseInt($("#lengthTo")[0].value),
-        limit: parseInt($("#wordQuantity")[0].value),
+        minLength: parseInt(document.getElementById("lengthFrom").value),
+        maxLength: parseInt(document.getElementById("lengthTo").value),
+        limit: parseInt(document.getElementById("wordQuantity").value),
         minCorpusCount: 1,
         maxCorpusCount: -1,
         minDictionaryCount: 1,
@@ -53,6 +53,7 @@ function getParameters() {
         apiUrl: "http://api.wordnik.com:80/v4/words.json/randomWords",
         key: "a7ed8b27e3cc25fb5200c0bb50702461a1decb749750db2f0"
     };
+    storeData();
 }
 
 /**
@@ -71,8 +72,10 @@ function getWordnikUrl(wordnikParameters) {
         + "&minLength=" + wordnikParameters.minLength
         + "&maxLength=" + wordnikParameters.maxLength
         + "&limit=1" /*+ parameters.limit*/
-        + "&api_key=" + wordnikParameters.key;
+        + "&api_key=" + wordnikParameters.key
+        + "&noCache=" + generateNumber(10);
 }
+
 //#endregion
 
 //#region generate
@@ -85,22 +88,21 @@ function generateWord(generateAddSymbols) {
         success: function (data) {
             if (data.length !== 0) {
                 generatedWords.push(generateCapitalization(parameters.capitalizationOption, data[0].word));
-				currentPassword === undefined ? currentPassword = generatedWords[generatedWords.length - 1] : currentPassword+=generatedWords[generatedWords.length - 1];
+                currentPassword === undefined ? currentPassword = generatedWords[generatedWords.length - 1] : currentPassword += generatedWords[generatedWords.length - 1];
                 if (generateAddSymbols)
-					generateAddSymbols();
+                    generateAddSymbols();
             }
         }
     });
 }
 
-function generateAddSymbols(){
-	if (parameters.symbolsScheme === "userDefined" || !parameters.generateWithSymbols)
-		symbolsUserDefined();
-	else if (parameters.symbolsScheme === "systemDefined"){
-		symbolsSystemDefined();		
-		}
-}
 
+function generateAddSymbols() {
+    if (parameters.symbolsScheme === "userDefined" || !parameters.generateWithSymbols)
+        symbolsUserDefined();
+    else if (parameters.symbolsScheme === "systemDefined")
+        symbolsSystemDefined();
+}
 /**
  * /
  * @param {string} capitalizationOption How the word will be capitalized, upper, camelcase, random, or lower.
@@ -128,42 +130,38 @@ function generateCapitalization(capitalizationOption, word) {
  */
 function generateNumber(howMany) {
     var numbers = "";
-    for (i = 0; i < howMany; i++) {
+    for (i = 0; i < howMany; i++)
         numbers += Math.floor(Math.random() * 10);
-    }
     return numbers;
 }
 
 function generatePassword() {
     currentPassword = "";
     generateUpdatePassword(currentPassword);
-    $("#generatedContent").text("Generating password...");
+    document.getElementById("generatedContent").style.display = "block";
+    document.getElementById("generatedContent").textContent = "Generating password...";
     generatedWords = [];
-    getParameters();
     if (parameters.wordnikParameters.limit > 0) {
-        for (i = 0; i < parameters.wordnikParameters.limit; i++) {
+        for (i = 0; i < parameters.wordnikParameters.limit; i++)
             generateWord(generateAddSymbols);
-        }
     }
-    else {
+    else
         generateAddSymbols();
-    }
 }
 
 function generateUpdatePassword(newPassword) {
-    $(passwordProgress).show();
-    $(passwordProgress).html("Current password is: " + newPassword);    
+    document.getElementById("passwordProgress").style.display = "block";
+    document.getElementById("passwordProgress").textContent = "Current password is: " + newPassword;
 }
 //#endregion
 
 //#region symbols
 function symbolsUserDefined() {
-	var symbolsToAdd = [];
+    var symbolsToAdd = [];
     if (generatedWords.length === parameters.wordnikParameters.limit) {
-
         //Shuffle the symbols
-		if (parameters.symbolsToAdd !== null)
-			symbolsToAdd = randomizeArray(parameters.symbolsToAdd);
+        if (parameters.symbolsToAdd !== null)
+            symbolsToAdd = randomizeArray(parameters.symbolsToAdd);
         var wordsWithSymbols = "";
         if (parameters.generateWithSymbols === true) {
             //If there is no words, set password as the symbols array
@@ -211,8 +209,8 @@ function symbolsUserDefined() {
             }
         }
         else {
-			for (i=0; i<generatedWords.length; i++)
-            wordsWithSymbols += generatedWords[i];
+            for (i = 0; i < generatedWords.length; i++)
+                wordsWithSymbols += generatedWords[i];
             currentPassword = wordsWithSymbols;
             generateUpdatePassword(currentPassword);
         }
@@ -222,37 +220,41 @@ function symbolsUserDefined() {
             currentPassword = wordsWithSymbols;
             generateUpdatePassword(currentPassword);
         }
-
-        $("#generatedContent").html("<b>YOUR PASSWORD:</b><br>" + wordsWithSymbols);
-        $("#generatedContent").show();
+        document.getElementById("generatedContent").innerHTML = "<b>YOUR PASSWORD:</b><br>" + wordsWithSymbols;
+        document.getElementById("generatedContent").style.display = "block";
+        document.getElementById("passwordProgress").style.display = "none";
     }
 }
 
-function symbolsSystemDefined() {	
-	
-	if (generatedWords.length === parameters.wordnikParameters.limit) {
-		var wordsWithSymbols = "";
-	var keyMap0 = ["a", "A", "E", "o", "O", "l"];
-	var keyMap1 = ["@", "4", "3", "0", "0","1"];
-	for (j=0; j<generatedWords.length; j++){
-		var word = generatedWords[j];
-		for (i=0; i<word.length; i++){
-			var index = keyMap0.indexOf(word[i]);
-			if (index >= 0 && Math.random() >= 0.5){
-				wordsWithSymbols += keyMap1[index];
-                currentPassword = wordsWithSymbols;
-                generateUpdatePassword(currentPassword);
-			}
-			else{
-				wordsWithSymbols+= word[i];
-                currentPassword = wordsWithSymbols;
-                generateUpdatePassword(currentPassword);
-				}
-			}
-		}
+function symbolsSystemDefined() {
 
-    $("#generatedContent").html("<b>YOUR PASSWORD:</b><br>" + wordsWithSymbols);
-    $("#generatedContent").show();
-	}
+    if (generatedWords.length === parameters.wordnikParameters.limit) {
+        var wordsWithSymbols = "";
+        var keyMap0 = ["a", "A", "E", "o", "O", "l"];
+        var keyMap1 = ["@", "4", "3", "0", "0", "1"];
+        for (j = 0; j < generatedWords.length; j++) {
+            var word = generatedWords[j];
+            for (i = 0; i < word.length; i++) {
+                var index = keyMap0.indexOf(word[i]);
+                if (index >= 0 && Math.random() >= 0.5) {
+                    wordsWithSymbols += keyMap1[index];
+                    currentPassword = wordsWithSymbols;
+                    generateUpdatePassword(currentPassword);
+                }
+                else {
+                    wordsWithSymbols += word[i];
+                    currentPassword = wordsWithSymbols;
+                    generateUpdatePassword(currentPassword);
+                }
+            }
+        }
+
+        document.getElementById("generatedContent").innerHTML = "<b>YOUR PASSWORD:</b><br>" + wordsWithSymbols;
+        document.getElementById("generatedContent").style.display = "block";
+        document.getElementById("passwordProgress").style.display = "none";
+    }
 }
-//#endregion
+$("select").change(getParameters);
+$("input").change(getParameters);
+
+        //#endregion
